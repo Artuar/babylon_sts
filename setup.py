c0de31a42d166b8c95ce55bbde246fc2b0b79171
artur <artuar1990@gmail.com>
2024-07-20 21:48:37 +0300

    # version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='babylon_sts',
     py_modules=["babylon_sts"],
-    version='0.1.2',
+    version='0.1.3',
     description='A library for audio processing with speech recognition and translation',
     author='Artur Rieznik',
     author_email='artuar1990@gmail.com',
